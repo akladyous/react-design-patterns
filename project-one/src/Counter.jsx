00d0529cd1f0a55a31a514a0c0ba655f0df03346
Boula Akladyous <akladyous@gmail.com>
2022-11-15 15:24:01 -0500

    project-one

--- project-one/src/Counter.jsx
+++ project-one/src/Counter.jsx
@@ -48,6 +48,7 @@ class Counter extends React.Component {
     }
 
     shouldComponentUpdate(nextProps, nextState) {
+        console.log('shouldComponentUpdate')
         /*
         shouldComponentUpdate() is invoked before rendering when new props or state are being received.
         Defaults to true. This method is not called for the initial render or when forceUpdate() is used.
