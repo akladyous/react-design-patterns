17b4fcf969e93bb18098733b80584a30631c6b17
Boula Akladyous <akladyous@gmail.com>
2022-11-21 16:03:44 -0500

    navbar tailwind

--- navbar/src/App.js
+++ navbar/src/App.js
@@ -41,7 +41,7 @@ function App() {
 
                     <li>
                         <a className="text-sm  font-bold" href="/">
-                            About Us
+                            About
                         </a>
                     </li>
 
