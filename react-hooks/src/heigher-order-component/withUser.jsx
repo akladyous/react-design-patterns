5b459a00c6d0117eef5fb822b66324e9b0372bf4
Boula Akladyous <akladyous@gmail.com>
2022-11-28 11:52:07 -0500

    heigher order component

--- react-hooks/src/heigher-order-component/withUser.jsx
+++ react-hooks/src/heigher-order-component/withUser.jsx
@@ -1,8 +1,15 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-export default function withUser() {
-    return (
-        <div>withUser</div>
-    )
+export default function withUser(Component, userId) {
+    return props => {
+        const [user, setUser] = useState(null);
+        useEffect(() => {
+            (async () => {
+                const response = await axios.get(`user/${userId}`)
+                setUser(response.data);
+            })();
+        }, []);
+        return <Component {...props} user={user} />
+    };
 }
