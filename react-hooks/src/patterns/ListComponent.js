6ab05d44d5dd799860a5415d2bc24ed648eeb67e
Boula Akladyous <akladyous@gmail.com>
2022-11-27 22:17:02 -0500

    hoocks

--- react-hooks/src/patterns/ListComponent.js
+++ react-hooks/src/patterns/ListComponent.js
@@ -5,23 +5,23 @@ export default function ListComponent(props) {
     const {
         items,
         resourceName,
-        itemComponent : ItemComponent,
+        itemComponent: ItemComponent,
     } = props || {};
-    const {loading} = props || false
-    console.log("loading: " , loading);
+    const { loading } = props || false
+    console.log("loading: ", loading);
     return (
         <>
-        {loading ? <p style={{backgroundColor: "red"}}>loading...</p> : null}
-        {
-            items && Array.isArray(items) ? 
-            items.map( (item, index) => (
-                <ItemComponent
-                    key={window.crypto.randomUUID()}
-                    { ...{ [resourceName]: item } }
-                />
-            ))
-            : null
-        }
+            {loading ? <p style={{ backgroundColor: "red" }}>loading...</p> : null}
+            {
+                items && Array.isArray(items) ?
+                    items.map((item, index) => (
+                        <ItemComponent
+                            key={window.crypto.randomUUID()}
+                            {...{ [resourceName]: item }}
+                        />
+                    ))
+                    : null
+            }
         </>
     )
 }
