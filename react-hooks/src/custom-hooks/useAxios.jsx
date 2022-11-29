f9e41024e88f84c90d583ce863fb715189169dd3
Boula Akladyous <akladyous@gmail.com>
2022-11-29 00:42:45 -0500

    axios

--- react-hooks/src/custom-hooks/useAxios.jsx
+++ react-hooks/src/custom-hooks/useAxios.jsx
@@ -7,8 +7,31 @@ export default function useAxios(props) {
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
 
+
+    const controller = new AbortController();
+
+    async function fetchData(url, params) {
+        const config = {
+            headers: { "Content-type": "application/json" },
+            signal: controller.signal
+        };
+        try {
+            const response = axios.get(url, config)
+            setData(response.data)
+        } catch (error) {
+            setError(error)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
     useEffect(() => {
 
+
+        return () => {
+            setIsLoading(false)
+            controller.abort()
+        }
     }, [])
     return (
         <div>useAxios</div>
