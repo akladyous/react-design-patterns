c0f8a2e07744a45ebcd9764d100c25002c06e497
Boula Akladyous <akladyous@gmail.com>
2023-04-16 21:34:49 -0400

    Untracked files on react

--- lifecycle/src/App.old.jsx
+++ lifecycle/src/App.old.jsx
@@ -1,45 +1,49 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import React from "react"
+import Counter from "./Counter.jsx"
+import ErrorBoundary from './ErrorBoundary.jsx'
 
-function App() {
-  const [count, setCount] = useState(0);
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        window._app = self
+        this.state = { mountCounter: false, counterStep: 0 }
+        this.mountCounter = this.mountCounter.bind(this)
+        this.umMountCounter = this.umMountCounter.bind(this)
 
-  return (
-    <div className='App'>
-      <div>
-        <a
-          href='https://vitejs.dev'
-          target='_blank'
-        >
-          <img
-            src={viteLogo}
-            className='logo'
-            alt='Vite logo'
-          />
-        </a>
-        <a
-          href='https://reactjs.org'
-          target='_blank'
-        >
-          <img
-            src={reactLogo}
-            className='logo react'
-            alt='React logo'
-          />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </div>
-  );
+    };
+    mountCounter() { this.setState({ mountCounter: true }) }
+    umMountCounter() { this.setState({ mountCounter: false }) }
+
+    changeCounterStep() { this.setState({ counterStep: Math.random() }) }
+    resetCounterStep() { this.setState({ counterStep: 0 }) }
+
+    render() {
+        return (
+            <div className="container mx-auto border-solid px-4">
+                <div className="mount-container">
+                    <button onClick={this.mountCounter} disabled={this.state.mountCounter}>Mount Counter</button>
+                    <button onClick={this.umMountCounter} disabled={!this.state.mountCounter}>UnMount Counter</button>
+                </div>
+                <div>
+                    <button onClick={this.changeCounterStep.bind(this)}>generate random step value</button>
+                </div>
+                {
+                    this.state.mountCounter
+                        ?
+                        <ErrorBoundary>
+                            <Counter
+                                counterStep={this.state.counterStep}
+                                resetCounterStep={this.resetCounterStep.bind(this)}
+                            />
+                        </ErrorBoundary>
+                        :
+                        null
+                }
+            </div>
+
+        )
+    }
 }
 
-export default App;
+
+export default App
