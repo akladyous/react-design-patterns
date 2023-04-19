250e2a000b90177fc44d2fcafb38617fe464a42b
Boula Akladyous <akladyous@gmail.com>
2023-04-19 16:02:05 -0400

    Untracked files on hooks

--- hooks/src/createRef/UsingFunctionComponnet.jsx
+++ hooks/src/createRef/UsingFunctionComponnet.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function UsingFunctionComponnet() {
-  const inputRef = React.createRef();
+  const inputRef = React.useRef(null);
   return (
     <div>
       <input
