2b6c92e9a078320a1714f290082ce4e2e93613f8
Boula Akladyous <akladyous@gmail.com>
2023-04-19 15:53:28 -0400

    Untracked files on hooks

--- hooks/src/forwardRef/Form.jsx
+++ hooks/src/forwardRef/Form.jsx
@@ -1,10 +1,18 @@
+import { useEffect, useRef } from 'react';
 import TextField from './TextField';
 
 export default function Form() {
+  const firstNameRef = useRef(null);
+
+  function updateFirstName(e) {
+    firstNameRef.current.focus();
+  }
+  useEffect(() => {}, []);
   return (
     <div className='mx-auto w-1/3 mt-10'>
       <form className=' border rounded-md mx-3 p-3'>
         <TextField
+          ref={firstNameRef}
           type='text'
           name='firstName'
           label='first name'
@@ -14,6 +22,13 @@ export default function Form() {
           name='lastName'
           label='last name'
         />
+        <button
+          type='button'
+          className='border px-3'
+          onClick={updateFirstName}
+        >
+          update first name
+        </button>
       </form>
     </div>
   );
--- hooks/src/forwardRef/TextField.jsx
+++ hooks/src/forwardRef/TextField.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
-export default function TextField({ type, name, classes, label, ...rest }) {
+function FormTextField({ type, name, classes, label, ...rest }, ref) {
   const className =
     'mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1';
+
   return (
     <>
       <label
@@ -15,6 +16,7 @@ export default function TextField({ type, name, classes, label, ...rest }) {
       </label>
 
       <input
+        ref={ref}
         type={type}
         name={name}
         className={classes ? classes : className}
@@ -23,3 +25,5 @@ export default function TextField({ type, name, classes, label, ...rest }) {
     </>
   );
 }
+const TextField = forwardRef(FormTextField);
+export default TextField;
