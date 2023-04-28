5e5ce193f1b2ddcac0f81a4845e66ea330aab24f
Boula Akladyous <akladyous@gmail.com>
2023-04-28 18:57:45 -0400

    Untracked files on hooks

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -6,13 +6,14 @@ import UseDebugValueDemo from './useDebugValue/UseDebugValueDemo';
 import DeferedDemo from './useDeferredValue/DeferedDemo';
 import Form from './forwardRef/Form';
 import ModalParent from './useImperativeHandle/ModalParent';
+import ReducerDemo from './useReducer/ReducerDemo';
 
 function App() {
   return (
     <div className='App px-5'>
       <h1 className='text-4xl text-center'>Main App</h1>
 
-      <StateDemo />
+      {/* <StateDemo /> */}
 
       {/* <ContextDemo /> */}
       {/* <Counter />  */}
@@ -21,6 +22,8 @@ function App() {
       {/* <Form /> */}
 
       {/* <ModalParent /> */}
+
+      <ReducerDemo />
     </div>
   );
 }
---
+++ hooks/src/useReducer/ReducerDemo.jsx
@@ -0,0 +1,11 @@
+import ReducerForm from './ReducerForm';
+
+export default function ReducerDemo(props) {
+  return (
+    <div className='border p-2 my-3 w-1/2'>
+      <>
+        <ReducerForm />
+      </>
+    </div>
+  );
+}
