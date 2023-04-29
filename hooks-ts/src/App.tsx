d20a213331437eaf595788c19078096e4f3f605a
Boula Akladyous <akladyous@gmail.com>
2023-04-29 01:27:55 -0400

    Untracked files on react

---
+++ hooks-ts/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className='m-5 border min-h-screen'>
+      <h1 className='text-xl font-bold text-center'>Hello world!</h1>
+    </main>
+  );
+}
+
+export default App;
