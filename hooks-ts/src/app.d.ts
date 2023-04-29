d7bec4489381c8a43fbc44a3dbec55a998c61524
Boula Akladyous <akladyous@gmail.com>
2023-04-29 14:02:09 -0400

    Untracked files on hooks-ts

--- hooks-ts/src/app.d.ts
+++ hooks-ts/src/app.d.ts
@@ -5,3 +5,27 @@ interface ClickHandler {
 }
 
 // typescript function type expression vs function call signatures
+
+type User = {
+  id: number | string;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
