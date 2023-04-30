5a5b2378ef7fd4d90270155b6c2673b591396dcf
Boula Akladyous <akladyous@gmail.com>
2023-04-30 13:40:40 -0400

    Untracked files on hooks-ts

--- hooks-ts/src/useContext/UserFormContext.tsx
+++ hooks-ts/src/useContext/UserFormContext.tsx
@@ -1,76 +1,47 @@
 import { useReducer } from 'react';
 
 const initialState: User = {
-  id: 0,
-  name: '',
-  userName: '',
+  id: '',
+  firstName: '',
+  lastName: '',
   email: '',
+  password: '',
   address: {
     street: '',
-    suite: '',
+    state: '',
     city: '',
-    zipcode: '',
+    zipCode: '',
     geo: {
       lat: '',
       lng: '',
     },
   },
-  phone: '',
-  website: '',
-  company: {
-    name: '',
-    catchPhrase: '',
-    bs: '',
-  },
 };
 
 const initializer = (state: User): User => {
-  return state;
+  return {
+    ...state,
+    id: crypto.randomUUID(),
+  };
 };
 function reducer(state: User, action: Action): User {
   switch (action.type) {
-    case 'SET_NAME':
-      return { ...state, name: action.payload };
-    case 'SET_EMAIL':
-      return { ...state, email: action.payload };
-    case 'SET_STREET':
-      return {
-        ...state,
-        address: { ...state.address, street: action.payload },
-      };
-    case 'SET_CITY':
-      return { ...state, address: { ...state.address, city: action.payload } };
-    case 'SET_ZIPCODE':
-      return {
-        ...state,
-        address: { ...state.address, zipcode: action.payload },
-      };
-    case 'SET_LAT':
-      return {
-        ...state,
-        address: {
-          ...state.address,
-          geo: { ...state.address.geo, lat: action.payload },
-        },
-      };
-    case 'SET_LNG':
-      return {
-        ...state,
-        address: {
-          ...state.address,
-          geo: { ...state.address.geo, lng: action.payload },
-        },
-      };
+    case ActionType.SET_VALUE:
+      return { ...state, [action.payload.name]: action.payload.value };
+
     default:
       return state;
   }
 }
 
 export default function UserFormContext(_props: {}) {
-  const [user, dispatch] = useReducer(reducer, initialState);
+  const [user, dispatch] = useReducer(reducer, initialState, initializer);
 
-  const handleChange = () => {
-    dispatch();
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: ActionType.SET_VALUE,
+      payload: { name: e.target.name, value: e.target.value },
+    });
   };
 
   return (
@@ -79,60 +50,168 @@ export default function UserFormContext(_props: {}) {
         <p>Context Component</p>
       </div>
       <form>
-        <div className='mb-2'>
-          <label
-            htmlFor='name'
-            className='block text-sm font-medium leading-6 text-gray-900 capitalize'
-          >
-            Name
-          </label>
-          <div className='mt-2'>
-            <input
-              id='name'
-              name='name'
-              type='text'
-              value={user.name}
-              className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 placeholder:mx-2 placeholder:capitalize placeholder:text-[.8rem]'
-              placeholder='name'
-            />
+        <div className=' grid grid-cols-2 gap-x-3'>
+          <div className='mb-2'>
+            <label
+              htmlFor='firstName'
+              className='block text-sm font-medium leading-6 text-gray-900 capitalize'
+            >
+              first name
+            </label>
+            <div className='mt-2'>
+              <input
+                id='firstName'
+                name='firstName'
+                type='text'
+                value={user.firstName}
+                onChange={handleChange}
+                className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 placeholder:mx-2 placeholder:capitalize placeholder:text-[.8rem]'
+                placeholder='first name'
+              />
+            </div>
+          </div>
+
+          <div className='mb-2'>
+            <label
+              htmlFor='lastName'
+              className='block text-sm font-medium leading-6 text-gray-900 capitalize'
+            >
+              last name
+            </label>
+            <div className='mt-2'>
+              <input
+                id='lastName'
+                name='lastName'
+                type='text'
+                value={user.lastName}
+                onChange={handleChange}
+                className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 placeholder:mx-2 placeholder:capitalize placeholder:text-[.8rem]'
+                placeholder='last name'
+              />
+            </div>
+          </div>
+          {/*  */}
+          <div className='mb-2'>
+            <label
+              htmlFor='email'
+              className='block text-sm font-medium leading-6 text-gray-900 capitalize'
+            >
+              email
+            </label>
+            <div className='mt-2'>
+              <input
+                id='email'
+                name='email'
+                type='email'
+                value={user.email}
+                onChange={handleChange}
+                className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 placeholder:mx-2 placeholder:capitalize placeholder:text-[.8rem]'
+                placeholder='email'
+              />
+            </div>
+          </div>
+          {/*  */}
+          <div className='mb-2'>
+            <label
+              htmlFor='email'
+              className='block text-sm font-medium leading-6 text-gray-900 capitalize'
+            >
+              password
+            </label>
+            <div className='mt-2'>
+              <input
+                id='password'
+                name='password'
+                type='password'
+                value={user.email}
+                onChange={handleChange}
+                className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 placeholder:mx-2 placeholder:capitalize placeholder:text-[.8rem]'
+                placeholder='password'
+              />
+            </div>
+          </div>
+        </div>
+        <div className=' grid grid-cols-1 gap-x-3'>
+          <h4>address</h4>
+          <div className='mb-2 sm:col-span-3'>
+            <label
+              htmlFor='street'
+              className='block text-sm font-medium leading-6 text-gray-900 capitalize'
+            >
+              street
+            </label>
+            <div className='mt-2'>
+              <input
+                id='street'
+                name='street'
+                type='text'
+                value={user.address.street}
+                onChange={handleChange}
+                className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 placeholder:mx-2 placeholder:capitalize placeholder:text-[.8rem]'
+                placeholder='street'
+              />
+            </div>
+          </div>
+          {/* ----------------- */}
+          <div className='mb-2 sm:col-span-1'>
+            <label
+              htmlFor='state'
+              className='block text-sm font-medium leading-6 text-gray-900 capitalize'
+            >
+              city
+            </label>
+            <div className='mt-2'>
+              <input
+                id='city'
+                name='city'
+                type='text'
+                value={user.address.city}
+                onChange={handleChange}
+                className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 placeholder:mx-2 placeholder:capitalize placeholder:text-[.8rem]'
+                placeholder='city'
+              />
+            </div>
+          </div>
+          <div className='mb-2 sm:col-span-1'>
+            <label
+              htmlFor='state'
+              className='block text-sm font-medium leading-6 text-gray-900 capitalize'
+            >
+              state
+            </label>
+            <div className='mt-2'>
+              <input
+                id='state'
+                name='state'
+                type='text'
+                value={user.address.state}
+                onChange={handleChange}
+                className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 placeholder:mx-2 placeholder:capitalize placeholder:text-[.8rem]'
+                placeholder='state'
+              />
+            </div>
+          </div>
+          <div className='mb-2 sm:col-span-1'>
+            <label
+              htmlFor='state'
+              className='block text-sm font-medium leading-6 text-gray-900 capitalize'
+            >
+              zip code
+            </label>
+            <div className='mt-2'>
+              <input
+                id='zipCode'
+                name='zipCode'
+                type='text'
+                value={user.address.zipCode}
+                onChange={handleChange}
+                className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 placeholder:mx-2 placeholder:capitalize placeholder:text-[.8rem]'
+                placeholder='zip Code'
+              />
+            </div>
           </div>
         </div>
         {/*  */}
-        <div className='mb-2'>
-          <label
-            htmlFor='userName'
-            className='block text-sm font-medium leading-6 text-gray-900 capitalize'
-          >
-            User Name
-          </label>
-          <div className='mt-2'>
-            <input
-              type='text'
-              name='userName'
-              id='userName'
-              className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 placeholder:mx-2 placeholder:capitalize placeholder:text-[.8rem]'
-              placeholder='user name'
-            />
-          </div>
-        </div>
-        {/*  */}
-        <div className='mb-2'>
-          <label
-            htmlFor='email'
-            className='block text-sm font-medium leading-6 text-gray-900 capitalize'
-          >
-            email
-          </label>
-          <div className='mt-2'>
-            <input
-              type='email'
-              name='email'
-              id='email'
-              className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 placeholder:mx-2 placeholder:capitalize placeholder:text-[.8rem]'
-              placeholder='email'
-            />
-          </div>
-        </div>
       </form>
     </section>
   );
--- hooks-ts/src/useContext/useContext.d.ts
+++ hooks-ts/src/useContext/useContext.d.ts
@@ -1,40 +1,37 @@
 type User = {
-  id: number;
-  name: string;
-  userName: string;
+  id: string;
+  firstName: string;
+  lastName: string;
   email: string;
+  password: string;
   address: {
     street: string;
-    suite: string;
     city: string;
+    state: string;
     zipcode: string;
     geo: {
       lat: string;
       lng: string;
     };
   };
-  phone: string;
-  website: string;
-  company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  };
 };
 
 enum ActionType {
-  SET_NAME = 'SET_NAME',
-  SET_EMAIL = 'SET_EMAIL',
-  SET_STREET = 'SET_STREET',
-  SET_CITY = 'SET_CITY',
-  SET_ZIPCODE = 'SET_ZIPCODE',
+  SET_VALUE = 'SET_VALUE',
+  SET_NESTED_VALUE = 'SET_NESTED_VALUE',
 }
 
+type PayloadType = {
+  name: string;
+  value: string;
+};
+
 type Action =
-  | { type: 'SET_NAME'; payload: string }
-  | { type: 'SET_EMAIL'; payload: string }
-  | { type: 'SET_STREET'; payload: string }
-  | { type: 'SET_CITY'; payload: string }
-  | { type: 'SET_ZIPCODE'; payload: string }
-  | { type: 'SET_LAT'; payload: string }
-  | { type: 'SET_LNG'; payload: string };
+  | {
+      type: ActionType.SET_VALUE;
+      payload: PayloadType;
+    }
+  | {
+      type: ActionType.SET_NESTED_VALUE;
+      payload: PayloadType;
+    };
