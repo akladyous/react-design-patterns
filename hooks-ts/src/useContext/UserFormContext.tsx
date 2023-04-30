7f9fd063fc184ffdcec6eb290f1b3ba1cdef9abb
Boula Akladyous <akladyous@gmail.com>
2023-04-30 11:48:16 -0400

    Untracked files on hooks-ts

--- hooks-ts/src/useContext/UserFormContext.tsx
+++ hooks-ts/src/useContext/UserFormContext.tsx
@@ -1,18 +1,4 @@
 import { useReducer } from 'react';
-enum ActionType {
-  SET_NAME = 'SET_NAME',
-  SET_EMAIL = 'SET_EMAIL',
-  SET_STREET = 'SET_STREET',
-  SET_CITY = 'SET_CITY',
-  SET_ZIPCODE = 'SET_ZIPCODE',
-}
-
-type Action =
-  | { type: ActionType.SET_NAME; payload: string }
-  | { type: ActionType.SET_EMAIL; payload: string }
-  | { type: ActionType.SET_STREET; payload: string }
-  | { type: ActionType.SET_CITY; payload: string }
-  | { type: ActionType.SET_ZIPCODE; payload: string };
 
 const initialState: User = {
   id: 0,
@@ -53,16 +39,16 @@ export default function UserFormContext(_props: {}) {
       <form>
         <div>
           <label
-            htmlFor='email'
+            htmlFor='name'
             className='block text-sm font-medium leading-6 text-gray-900'
           >
-            Email
+            Name
           </label>
           <div className='mt-2'>
             <input
-              type='email'
-              name='email'
-              id='email'
+              type='name'
+              name='name'
+              id='name'
               className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
               placeholder='you@example.com'
             />
--- hooks-ts/src/useContext/useContext.d.ts
+++ hooks-ts/src/useContext/useContext.d.ts
@@ -1,3 +1,27 @@
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
+
 enum ActionType {
   SET_NAME = 'SET_NAME',
   SET_EMAIL = 'SET_EMAIL',
