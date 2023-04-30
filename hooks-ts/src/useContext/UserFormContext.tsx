cac47a2e1fa955fcaf56a5919f54f695ca82d5ab
Boula Akladyous <akladyous@gmail.com>
2023-04-30 11:31:57 -0400

    Untracked files on hooks-ts

--- hooks-ts/src/useContext/UserFormContext.tsx
+++ hooks-ts/src/useContext/UserFormContext.tsx
@@ -1,4 +1,18 @@
 import { useReducer } from 'react';
+enum ActionType {
+  SET_NAME = 'SET_NAME',
+  SET_EMAIL = 'SET_EMAIL',
+  SET_STREET = 'SET_STREET',
+  SET_CITY = 'SET_CITY',
+  SET_ZIPCODE = 'SET_ZIPCODE',
+}
+
+type Action =
+  | { type: ActionType.SET_NAME; payload: string }
+  | { type: ActionType.SET_EMAIL; payload: string }
+  | { type: ActionType.SET_STREET; payload: string }
+  | { type: ActionType.SET_CITY; payload: string }
+  | { type: ActionType.SET_ZIPCODE; payload: string };
 
 const initialState: User = {
   id: 0,
