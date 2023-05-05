d5b3621f5da2dac0cc11029de540f0ddd2e0aa9f
Boula Akladyous <akladyous@gmail.com>
2023-05-05 09:35:16 -0400

    Untracked files on hooks

--- hooks/src/useTransition/WordList.jsx
+++ hooks/src/useTransition/WordList.jsx
@@ -1,8 +1,30 @@
-import { useDeferredValue } from 'react';
+import { useDeferredValue, useEffect, useTransition } from 'react';
+import { fetchData } from '../util/fetchData';
 
 export default function WordList({ query }) {
   const [photos, setPhotos] = useState([]);
+  const [isPending, startTransition] = useTransition();
   const deferedQuery = useDeferredValue(query);
 
-  return <section>{}</section>;
+  useEffect(() => {
+    setTimeout(() => {
+      startTransition(() => {
+        fetchData().then((data) => setPhotos(data.photos));
+      });
+    });
+  }, []);
+  return (
+    <section>
+      <div className='my-2 p-2 border rounded-md'>
+        <ul>
+          {isPending ? <p className=' text-orange-500'>loading...</p> : null}
+          {photos
+            .filter((photo) => photo.title.includes(query))
+            .map((photo) => (
+              <li key={photo.id}>{photo.title}</li>
+            ))}
+        </ul>
+      </div>
+    </section>
+  );
 }
