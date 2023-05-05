a2bfba359baf421df4658dc695ffa1e4febcdac4
Boula Akladyous <akladyous@gmail.com>
2023-05-05 09:21:26 -0400

    Untracked files on hooks

--- hooks/src/useTransition/WordList.jsx
+++ hooks/src/useTransition/WordList.jsx
@@ -1,6 +1,8 @@
 import { useDeferredValue } from 'react';
 
 export default function WordList({ query }) {
+  const [photos, setPhotos] = useState([]);
   const deferedQuery = useDeferredValue(query);
-  return <section></section>;
+
+  return <section>{}</section>;
 }
--- hooks/src/util/fetchData.js
+++ hooks/src/util/fetchData.js
@@ -1,10 +1,8 @@
 import { photos } from '../data/photos';
 export function fetchData(timeout = 3000) {
-  console.log(new Date().toISOString());
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve({ photos });
-      console.log(new Date().toISOString());
     }, timeout);
   });
 }
