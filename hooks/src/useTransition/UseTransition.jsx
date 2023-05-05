bb85f3bab4bf10d9b73e5b3bfcc3fcae05d24734
Boula Akladyous <akladyous@gmail.com>
2023-05-05 09:44:04 -0400

    Untracked files on hooks

--- hooks/src/useTransition/UseTransition.jsx
+++ hooks/src/useTransition/UseTransition.jsx
@@ -6,18 +6,6 @@ export default function UseTransition() {
 
   const handleChange = (e) => {
     setQuery(e.target.value);
-    // startTransition(() => {
-    //   const _words = photos
-    //     .map((p) => p.title)
-    //     .join(' ')
-    //     .split(' ');
-    //   for (let i = _words.length - 1; i > 0; i--) {
-    //     const j = Math.floor(Math.random() * (i + 1));
-    //     [_words[i], _words[j]] = [_words[j], _words[i]];
-    //   }
-    //   // (Math.floor(Math.random() * (i + 1)) / 2) * Math.sqrt(i + j);
-    //   setWords(_words);
-    // });
   };
 
   return (
--- hooks/src/util/titlesToWords.js
+++ hooks/src/util/titlesToWords.js
@@ -1,10 +1,14 @@
 import { photos } from '../data/photos';
 
-const _words = photos
-  .map((p) => p.title)
-  .join(' ')
-  .split(' ');
-for (let i = _words.length - 1; i > 0; i--) {
-  const j = Math.floor(Math.random() * (i + 1));
-  [_words[i], _words[j]] = [_words[j], _words[i]];
-}
+export const titleToWords = () => {
+  const _words = photos
+    .map((p) => p.title)
+    .join(' ')
+    .split(' ');
+  for (let i = _words.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [_words[i], _words[j]] = [_words[j], _words[i]];
+  }
+  // (Math.floor(Math.random() * (i + 1)) / 2) * Math.sqrt(i + j);
+  return _words;
+};
