aace57a8c89ddf3916310c412027eaa42cfcc08b
Boula Akladyous <akladyous@gmail.com>
2023-05-13 13:18:01 -0400

    Untracked files on react

--- react-patterns-nextjs/src/components/RegularList.tsx
+++ react-patterns-nextjs/src/components/RegularList.tsx
@@ -1,5 +1,1 @@
-export default function RegularList(props: {}) {
-  const { items, resourceName, itemComponent } = props;
-
-  return <></>;
-}
+import { users } from '@/data/users';
--- react-patterns-nextjs/src/components/test.js
+++ react-patterns-nextjs/src/components/test.js
@@ -5,6 +5,8 @@ resourceName: string
 itemComponent: is a react Component
 
 */
+
+
 export const RegularList = ({
   items,
   resourceName,
@@ -13,8 +15,18 @@ export const RegularList = ({
   return (
     <>
       {items.map((item) => (
-        <ItemComponent key={crypto.randomUUID()} />
+        <ItemComponent
+          key={crypto.randomUUID()}
+          {...{ [resourceName]: item }}
+        />
       ))}
     </>
   );
 };
+
+try again
+
+convert to typescript and consider react container component pattern with the following constrains:
+  items: list of objects
+  resourceName: string
+  itemComponent: by mapping over items, return the ItemComponent with React.key assigned on each loop as well as the resourceName
