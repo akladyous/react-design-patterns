883e4512cbda097733c7ca3ae52a9d13ad6e99a5
Boula Akladyous <akladyous@gmail.com>
2023-05-13 12:23:21 -0400

    Untracked files on react

--- react-patterns-nextjs/src/components/RegularList.tsx
+++ react-patterns-nextjs/src/components/RegularList.tsx
@@ -1,52 +1,5 @@
-import React from 'react';
+export default function RegularList(props: {}) {
+  const { items, resourceName, itemComponent } = props;
 
-interface RegularListProps<T> {
-  items: T[];
-  resourceName: string;
-  itemComponent: React.FC<{ key: string; resourceName: string }>;
+  return <></>;
 }
-
-export function RegularList<T>(props: RegularListProps<T>): JSX.Element {
-  const { items, resourceName, itemComponent: ItemComponent } = props;
-  return (
-    <>
-      {items.map((item, index) => (
-        <ItemComponent
-          key={crypto.randomUUID()}
-          resourceName={resourceName}
-          {...{ [resourceName]: item }}
-        />
-      ))}
-    </>
-  );
-}
-
-// Define a sample component that will render each item
-const SampleItemComponent: React.FC<{ key: string; resourceName: string }> = ({
-  key,
-  resourceName,
-}) => {
-  return (
-    <div>
-      {resourceName}: {key}
-    </div>
-  );
-};
-
-// Example usage of RegularList
-const App = () => {
-  const items = ['Item 1', 'Item 2', 'Item 3'];
-  const resourceName = 'Resource';
-
-  return (
-    <div>
-      <RegularList
-        items={items}
-        resourceName={resourceName}
-        itemComponent={SampleItemComponent}
-      />
-    </div>
-  );
-};
-
-export default App;
