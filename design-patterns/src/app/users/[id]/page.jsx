2e91f3dea92996e2a970c3e3e98dbecaf9ea581f
Boula Akladyous <akladyous@gmail.com>
2023-05-13 19:15:15 -0400

    Untracked files on react

--- design-patterns/src/app/users/[id]/page.jsx
+++ design-patterns/src/app/users/[id]/page.jsx
@@ -7,7 +7,7 @@ export default async function UserInfo({ params: { id }, searchParams }) {
   console.log('user : ', user);
 
   return user ? (
-    <div>
+    <div className='mx-2 p-2'>
       {/* <p>{JSON.stringify(user, null, 2)}</p> */}
       <p>{user.name}</p>
     </div>
