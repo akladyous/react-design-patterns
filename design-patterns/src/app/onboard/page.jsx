95ac6094140de80c99cd32851b1e68d7f07cee49
Boula Akladyous <akladyous@gmail.com>
2023-05-14 15:16:29 -0400

    Untracked files on react

--- design-patterns/src/app/onboard/page.jsx
+++ design-patterns/src/app/onboard/page.jsx
@@ -1,8 +1,24 @@
+import StepOne from './onboard/StepOne';
+import StepTwo from './onboard/StepTwo';
+import StepThree from './onboard/StepThree';
+import OnBoardingData from '@/components/OnBoardingData';
+
 export default function OnBoardHome({ children }) {
+  const [onBoardingData, setOnBoardingData] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const onNext = (stepData) => {
+    setOnBoardingData({ ...onBoardingData, ...stepData });
+    setCurrentIndex(currentIndex + 1);
+  };
+
   return (
-    <section>
-      <p>OnBoard Home Page</p>
-      {children}
-    </section>
+    <>
+      <OnBoardingData>
+        <StepOne />
+        <StepTwo />
+        <StepThree />
+      </OnBoardingData>
+    </>
   );
 }
