be6a14331e240326880a960617bbd35877c3c37b
Boula Akladyous <akladyous@gmail.com>
2023-05-14 12:34:28 -0400

    Untracked files on react

--- design-patterns/src/app/faker/page.jsx
+++ design-patterns/src/app/faker/page.jsx
@@ -1,12 +1,6 @@
-import { faker } from '@faker-js/faker';
 import { generateFakeUsers, sampleUser } from '@/lib/fakeUsers';
 
 export default async function Faker(props) {
-  // const randomName = faker.person.fullName(); // Rowan Nikolaus
-  // const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
-  // const female = faker.person.firstName('female');
-  // const webSite = faker.internet.url({ protocol: 'https', appendSlash: false });
-  // const user = sampleUser();
   const user = await generateFakeUsers();
   console.log(user.length);
   return (
