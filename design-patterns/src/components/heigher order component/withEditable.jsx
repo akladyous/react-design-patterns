d96d2085e999a356b8cfb0af09c63fb044033c8b
Boula Akladyous <akladyous@gmail.com>
2023-05-15 17:17:28 -0400

    Untracked files on react

---
+++ design-patterns/src/components/heigher order component/withEditable.jsx
@@ -0,0 +1,6 @@
+export const withEditable = (Component, resourcePath, resourceName) => {
+  return (props) => {
+    const [oridinalData, setOriginalData] = useState(null);
+    const [data, setData] = useState(null);
+  };
+};
