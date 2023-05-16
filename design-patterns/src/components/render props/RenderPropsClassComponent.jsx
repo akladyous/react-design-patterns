307d716a19c953db6eeb9a563b02c1326994dd8a
Boula Akladyous <akladyous@gmail.com>
2023-05-16 13:35:55 -0400

    Untracked files on react

--- design-patterns/src/components/render props/RenderPropsClassComponent.jsx
+++ design-patterns/src/components/render props/RenderPropsClassComponent.jsx
@@ -12,3 +12,44 @@ class Cat extends React.Component {
     );
   }
 }
+
+class Mouse extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.state = { x: 0, y: 0 };
+  }
+
+  handleMouseMove(event) {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY,
+    });
+  }
+
+  render() {
+    return (
+      <div
+        style={{ height: '100vh' }}
+        onMouseMove={this.handleMouseMove}
+      >
+        {/*
+          Instead of providing a static representation of what <Mouse> renders,
+          use the `render` prop to dynamically determine what to render.
+        */}
+        {this.props.render(this.state)}
+      </div>
+    );
+  }
+}
+
+class MouseTracker extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Move the mouse around!</h1>
+        <Mouse render={(mouse) => <Cat mouse={mouse} />} />
+      </div>
+    );
+  }
+}
