fe1bfadfbbeec9c9fcb485d45a84fda825aa45ea
Boula Akladyous <akladyous@gmail.com>
2023-05-16 11:32:18 -0400

    Untracked files on react

---
+++ design-patterns/src/app/fas/page.jsx
@@ -0,0 +1,11 @@
+export default function FunctionAsChild({ children }) {
+  return (
+    <>
+      <section>
+        <h3>Container Component Home</h3>
+
+        {children}
+      </section>
+    </>
+  );
+}
