aa191d1c60d0d927e8506f6d457ac5acc3aaea06
Boula Akladyous <akladyous@gmail.com>
2023-05-16 13:36:10 -0400

    Untracked files on react

--- design-patterns/src/components/render props/RenderPropsClassComponent.jsx
+++ design-patterns/src/components/render props/RenderPropsClassComponent.jsx
@@ -53,3 +53,20 @@ class MouseTracker extends React.Component {
     );
   }
 }
+
+function withMouse(Component) {
+  return class extends React.Component {
+    render() {
+      return (
+        <Mouse
+          render={(mouse) => (
+            <Component
+              {...this.props}
+              mouse={mouse}
+            />
+          )}
+        />
+      );
+    }
+  };
+}
