cd8e1903c958a3b3424f0fb7de26bfd75c6dafd2
Boula Akladyous <akladyous@gmail.com>
2023-05-16 11:49:14 -0400

    Untracked files on react

--- design-patterns/src/app/fas/page.jsx
+++ design-patterns/src/app/fas/page.jsx
@@ -1,11 +1,21 @@
 export default function FunctionAsChild({ children }) {
+  const text =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quos numquam, a maxime recusandae repellat tenetur asperiores nihil animi nostrum vero explicabo itaque laborum laboriosam eaque voluptas eveniet molestiae iusto?';
+  const isFunctionChild = typeof children === 'function';
   return (
     <>
-      <section>
-        <h3>Container Component Home</h3>
-
-        {children}
-      </section>
+      <div>
+        <p>
+          {isFunctionChild ? (
+            children(text)
+          ) : (
+            <>
+              {text}
+              {children}
+            </>
+          )}
+        </p>
+      </div>
     </>
   );
 }
--- design-patterns/src/app/page.js
+++ design-patterns/src/app/page.js
@@ -1,3 +1,7 @@
+'use client';
+
+import FunctionAsChild from './fas/page';
+
 export default function Home({ children }) {
   return (
     <section
@@ -5,6 +9,12 @@ export default function Home({ children }) {
       className='min-h-screen mx-2 p-2 border'
     >
       <h3>Home Page</h3>
+
+      <div className='border h-52 ml-2 my-2 p-2'>
+        <h3>Function As Child</h3>
+
+        <FunctionAsChild>{(text) => <strong>{text}</strong>}</FunctionAsChild>
+      </div>
       {children}
     </section>
   );
