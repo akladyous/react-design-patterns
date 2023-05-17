2107988bf2d634744438f4a8b89878dc8b775dd4
Boula Akladyous <akladyous@gmail.com>
2023-05-17 13:28:04 -0400

    Untracked files on react

--- design-patterns/src/components/render props/counter-example/Counter.jsx
+++ design-patterns/src/components/render props/counter-example/Counter.jsx
@@ -3,11 +3,11 @@ import { useState } from 'react';
 export default function Counter({ render, initialValue = 0 }) {
   const [count, setCount] = useState(initialValue);
 
-  const incCount = () => {
-    setCount((_count) => _count + 1);
+  const incCount = (v) => {
+    setCount((_count) => (_count + v ? v : 1));
   };
   const decCount = (v) => {
-    setCount((_count) => _count - 1);
+    setCount((_count) => (_count - v ? v : 1));
   };
 
   return <>{render(count, incCount, decCount)}</>;
