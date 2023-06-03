4a1992b16c54ce548e332de7df49c0586b85a98e
Boula Akladyous <akladyous@gmail.com>
2023-06-03 17:19:21 -0400

    Untracked files on react

---
+++ hooks/src/components/Counter.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+export default function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  function increment(value) {
+    setCounter((c) => c + 1);
+  }
+
+  function decrement(value) {
+    setCounter((c) => c - 1);
+  }
+
+  return (
+    <main className='border p-2 my-3' id='counter'>
+      <p className='text-center'>{counter}</p>
+      <div className='flex'>
+        <button
+          className='border rounded-md bg-slate-100 px-5 mx-auto'
+          onClick={increment}
+        >
+          +
+        </button>
+        <button
+          className='border rounded-md bg-slate-100 px-5 mx-auto'
+          onClick={decrement}
+        >
+          -
+        </button>
+      </div>
+    </main>
+  );
+}
