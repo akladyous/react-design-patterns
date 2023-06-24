6cf0a9df3b3b664f0adb8fe7ef75ccd959e1aa93
Boula Akladyous <akladyous@gmail.com>
2023-06-24 11:44:40 -0400

    Untracked files on hooks

--- hooks/src/useState/CounterOne.jsx
+++ hooks/src/useState/CounterOne.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useEffect } from 'react';
+import { ReactDOM, unstable_batchedUpdates } from 'react';
+import { useState, useRef } from 'react';
 
 export default function CounterOne(props) {
   const [counter, setCounter] = useState(0);
 
-  const increment = () => setCounter((v) => v + 1);
+  const increment = () => {
+    setCounter((v) => v + 1);
+  };
   const decrement = () => setCounter((v) => v - 1);
 
   const buttonElement = React.createElement(
@@ -12,9 +15,18 @@ export default function CounterOne(props) {
     {
       className: 'border rounded-md bg-slate-100 px-5 mx-auto',
       onClick: decrement,
+      key: '1234',
     },
     '-',
   );
+  console.log('-'.repeat(50));
+  console.log(buttonElement);
+  console.log('-'.repeat(50));
+
+  useEffect(() => {
+    console.log('rerender');
+    console.log('counter : ', counter);
+  }, [counter]);
 
   return (
     <section id='counterOne' className='border p-2 my-3 w-2/3 mx-auto'>
