c0d3fe1d3735e329d7fde1d2bb8ba2ad95a07ee5
Boula Akladyous <akladyous@gmail.com>
2023-06-29 16:32:55 -0400

    Untracked files on react-design-patterns

--- react-design-patterns/src/App.jsx
+++ react-design-patterns/src/App.jsx
@@ -9,5 +9,3 @@ function App() {
 }
 
 export default App;
-
-const Text = ({ text }) => <p>{text}</p>;
--- react-design-patterns/src/pattern/components/render-props/RenderPropOneDemo.jsx
+++ react-design-patterns/src/pattern/components/render-props/RenderPropOneDemo.jsx
@@ -1,1 +1,39 @@
-export default function RenderPropOneDemo() {}
+import RenderPropOne from './RenderPropOne';
+
+export default function RenderPropOneDemo({ counter, increment, decrement }) {
+  return (
+    <RenderPropOne
+      render={(counter, increment, decrement) => {
+        return (
+          <main className='border-2 border-red-500 p-2 my-3 w-1/3 mx-auto'>
+            <h4 className='text-center'>Counter One</h4>
+
+            <div className='' id='counter'>
+              <div className='flex my-2'>
+                <button
+                  className='border rounded-md bg-slate-100 px-5 mx-auto'
+                  onClick={(_e) => {
+                    increment(value);
+                  }}
+                >
+                  +
+                </button>
+                <p className='border rounded-lg text-center max-w-fit px-4'>
+                  {counter}
+                </p>
+                <button
+                  className='border rounded-md bg-slate-100 px-5 mx-auto'
+                  onClick={(_e) => {
+                    decrement(value);
+                  }}
+                >
+                  -
+                </button>
+              </div>
+            </div>
+          </main>
+        );
+      }}
+    />
+  );
+}
