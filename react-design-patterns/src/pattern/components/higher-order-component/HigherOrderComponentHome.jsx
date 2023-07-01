64c3517964563a9fd09ae2ac078ec7b2aa70eaac
Boula Akladyous <akladyous@gmail.com>
2023-06-30 23:50:11 -0400

    Untracked files on react-design-patterns

--- react-design-patterns/src/pattern/components/higher-order-component/HigherOrderComponentHome.jsx
+++ react-design-patterns/src/pattern/components/higher-order-component/HigherOrderComponentHome.jsx
@@ -1,13 +1,15 @@
 import HigherOrderComponentOneDemo from './implementation-one/HigherOrderComponentOneDemo';
 import HigherOrderComponentTwoDemo from './implementation-two/HigherOrderComponentTwoDemo';
 import HigherOrderComponentThreeDemo from './implementation-three/HigherOrderComponentThreeDemo';
+import HigherOrderComponentFourDemo from './implementation-four/HigherOrderComponentFourDemo';
 
 export default function HigherOrderComponentHome() {
   return (
     <main>
       {/* <HigherOrderComponentOneDemo /> */}
       {/* <HigherOrderComponentTwoDemo /> */}
-      <HigherOrderComponentThreeDemo />
+      {/* <HigherOrderComponentThreeDemo /> */}
+      <HigherOrderComponentFourDemo />
     </main>
   );
 }
