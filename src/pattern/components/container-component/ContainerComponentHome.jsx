6144b1ba82935c395a3ec301d5a1440f93e1a2e5
Boula Akladyous <akladyous@gmail.com>
2023-07-04 01:08:32 -0400

    Untracked files on react-design-patterns

--- src/pattern/components/container-component/ContainerComponentHome.jsx
+++
@@ -1,9 +0,0 @@
-import ContainerComponentOneDemo from './implementation-one/ContainerComponentOneDemo';
-
-export default function ContainerComponentHome() {
-  return (
-    <>
-      <ContainerComponentOneDemo />
-    </>
-  );
-}
